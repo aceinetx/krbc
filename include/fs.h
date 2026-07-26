41293107ebaa0832c6972e979edabac986d64203
aceinetx <mouselin82@gmail.com>
2026-07-26 21:23:49 +0300

    simplify the project structure
    no need for cmake

--- include/fs.h
+++
@@ -1,8 +0,0 @@
-#ifndef FS_H
-#define FS_H
-
-#include <krbc.h>
-
-byte* fs_read(const char* filename, dword* out_size);
-
-#endif
--- krbc.c
+++ krbc.c
@@ -1,4 +1,53 @@
-#include <krbc.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef uint8_t byte;
+typedef uint16_t word;
+typedef uint32_t dword;
+
+byte* fs_read(const char* filename, dword* out_size) {
+	FILE* file;
+	long fileSize;
+	byte* buffer;
+	dword bytesRead;
+
+	file = fopen(filename, "rb");
+	if (!file) {
+		return NULL;
+	}
+
+	fseek(file, 0, SEEK_END);
+	fileSize = ftell(file);
+	fseek(file, 0, SEEK_SET);
+
+	if (fileSize < 0) {
+		fclose(file);
+		return NULL;
+	}
+
+	buffer = (byte*)malloc(fileSize);
+	if (!buffer) {
+		fclose(file);
+		return NULL;
+	}
+
+	bytesRead = fread(buffer, 1, fileSize, file);
+	if (bytesRead != fileSize) {
+		free(buffer);
+		fclose(file);
+		return NULL;
+	}
+
+	fclose(file);
+
+	if (out_size) {
+		*out_size = (dword)fileSize;
+	}
+
+	return buffer;
+}
 
 FILE* ofd;
 dword loop_stack[128];
